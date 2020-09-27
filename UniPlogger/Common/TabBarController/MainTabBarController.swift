22beb939d64b9d16187e82a3f32c5f22bf104938
ByeonggeunSon <qudrms007@naver.com>
2020-09-27 20:27:26 +0900

    기본 탭바 지정 및 탭바 아이콘 추가
    아이콘 추가(아이콘 변경 필요)

--- UniPlogger/Common/TabBarController/MainTabBarController.swift
+++ UniPlogger/Common/TabBarController/MainTabBarController.swift
@@ -26,10 +26,18 @@ class MainTabBarController: UITabBarController {
 extension MainTabBarController {
     private func configuration() {
         self.view.backgroundColor = .white
+        self.tabBar.selectedImageTintColor = UIColor(red: 95/255, green: 116/255, blue: 244/255, alpha: 1)
+        self.tabBar.unselectedItemTintColor = UIColor(red: 206/255, green: 209/255, blue: 214/255, alpha: 1)
     }
     
     private func setupView() {
-        // addSubview(view)
+        setupChallengeViewController()
+        setupQuestViewController()
+        setupPloggingViewController()
+        setupLogViewController()
+        setupMyViewController()
+        
+        self.selectedIndex = 2
     }
     
     private func setupLayout() {
@@ -41,5 +49,40 @@ extension MainTabBarController {
     
     private func updateView() {
         
+    }
+    
+    func setupChallengeViewController(){
+        let challengeItem = UITabBarItem(title: "챌린지", image: UIImage(named: "tabbar_challenge"), tag: 0)
+        let vc = UIViewController()
+        vc.tabBarItem = challengeItem
+        self.addChild(vc)
+    }
+    
+    func setupQuestViewController(){
+        let questItem = UITabBarItem(title: "퀘스트", image: UIImage(named: "tabbar_quest"), tag: 1)
+        let vc = UIViewController()
+        vc.tabBarItem = questItem
+        self.addChild(vc)
+    }
+    
+    func setupPloggingViewController(){
+        let ploggingItem = UITabBarItem(title: "플로깅", image: UIImage(named: "tabbar_plogging"), tag: 2)
+        let vc = UIViewController()
+        vc.tabBarItem = ploggingItem
+        self.addChild(vc)
+    }
+    
+    func setupLogViewController(){
+        let logItem = UITabBarItem(title: "로그", image: UIImage(named: "tabbar_log"), tag: 3)
+        let vc = UIViewController()
+        vc.tabBarItem = logItem
+        self.addChild(vc)
+    }
+    
+    func setupMyViewController(){
+        let myItem = UITabBarItem(title: "마이", image: UIImage(named: "tabbar_my"), tag: 4)
+        let vc = UIViewController()
+        vc.tabBarItem = myItem
+        self.addChild(vc)
     }
 }
