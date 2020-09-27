979d15229e89518a4a30336c6669bbcc2c0f076f
Steve <qudrms007@naver.com>
2020-09-27 19:44:28 +0900

    Add Initial Project
    .gitignore, UniPlogger Project

---
+++ UniPlogger/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UniPlogger
+//
+//  Created by 손병근 on 2020/09/27.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
