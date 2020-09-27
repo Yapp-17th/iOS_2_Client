583f80f1a7f8bee95feb82a50596be4cddc39b75
ByeonggeunSon <qudrms007@naver.com>
2020-09-27 20:03:40 +0900

    메인 탭바 추가

--- UniPlogger/AppDelegate.swift
+++ UniPlogger/AppDelegate.swift
@@ -11,13 +11,13 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
-
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
 }
 
--- UniPlogger/Common/TabBarController/MainTabBarController.swift
+++ UniPlogger/Common/TabBarController/MainTabBarController.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2020 손병근. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    // MARK: View
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configuration()
+        setupView()
+        setupLayout()
+    }
+}
+
+extension MainTabBarController {
+    private func configuration() {
+        self.view.backgroundColor = .white
+    }
+    
+    private func setupView() {
+        // addSubview(view)
+    }
+    
+    private func setupLayout() {
+        self.tabBar.barTintColor = .white
+        self.tabBar.layer.cornerRadius = 22
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    private func updateView() {
+        
+    }
+}
