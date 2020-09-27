71143487914d38f31344b331daba1c538b4e9c73
ByeonggeunSon <qudrms007@naver.com>
2020-09-27 21:00:07 +0900

    Common Model 추가 및 폴더 구조 정리

---
+++ UniPlogger/Model/Common.swift
@@ -0,0 +1,17 @@
+//
+//  Common.swift
+//  UniPlogger
+//
+//  Created by 손병근 on 2020/09/27.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
+
+enum Common{
+    enum CommonError: Error{
+        case server(String)
+        case local(String)
+        case error(Error)
+    }
+}
