80bdfcd4b894b7365c644cd0ad6ba19b5eaaa63b
woongs <ccw1021@naver.com>
2020-10-03 08:09:08 +0900

    Implement QuestInteractor

--- UniPlogger/Scene/Quest/QuestInteractor.swift
+++ UniPlogger/Scene/Quest/QuestInteractor.swift
@@ -9,13 +9,24 @@
 import Foundation
 
 protocol QuestBusinessLogic {
-    
+    func fetchQuest(request: QuestModels.Reqeust)
 }
 
 protocol QuestDataStore {
-    
+    var questList: [Quest] { get set }
 }
 
-class QuestInteractor {
-    
+class QuestInteractor: QuestDataStore {
+    var presenter: QuestPresentationLogic?
+    var worker: QuestWorker?
+    var questList = [Quest]()
+}
+
+extension QuestInteractor: QuestBusinessLogic {
+    func fetchQuest(request: QuestModels.Reqeust) {
+        worker = QuestWorker()
+        let list = worker?.questData(state: request.state)
+        let response = QuestModels.Response(questList: list ?? [])
+        presenter?.presentQuestList(response: response)
+    }
 }
