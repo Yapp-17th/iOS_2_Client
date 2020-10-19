9e797d17bf8cef3ec57108bfa62d8607c06a8bd1
woongs <ccw1021@naver.com>
2020-10-20 01:21:05 +0900

    Fix QuestWorker
    State에 따라 불러오는 게 아니고 모두 fetch 해오는 시나리오 더미 데이터

--- UniPlogger/Scene/Quest/QuestWorker.swift
+++ UniPlogger/Scene/Quest/QuestWorker.swift
@@ -10,18 +10,28 @@ import Foundation
 
 class QuestWorker {
     
-    func questData(state: QuestState) -> [Quest] {
-        data(for: state)
-    }
-    
-    func data(for state: QuestState) -> [Quest] {
+    func fetchData() -> [Quest] {
         let data: [Quest] = [
-            Quest(id: "0", title: "학습퀘스트", content: "", category: .training, state: state),
-            Quest(id: "1", title: "목표달성형퀘스트", content: "", category: .routine, state: state),
-            Quest(id: "2", title: "목표달성형퀘스트", content: "", category: .routine, state: state),
-            Quest(id: "3", title: "목표달성형퀘스트", content: "", category: .routine, state: state),
-            Quest(id: "4", title: "목표달성형퀘스트", content: "", category: .routine, state: state),
-            Quest(id: "5", title: "목표달성형퀘스트", content: "", category: .routine, state: state)
+            Quest(id: "0", title: "학습퀘스트", content: "", category: .training, state: .doing),
+            Quest(id: "1", title: "목표달성형퀘스트", content: "", category: .routine, state: .doing),
+            Quest(id: "2", title: "목표달성형퀘스트", content: "", category: .routine, state: .doing),
+            Quest(id: "3", title: "목표달성형퀘스트", content: "", category: .routine, state: .doing),
+            Quest(id: "4", title: "목표달성형퀘스트", content: "", category: .routine, state: .doing),
+            Quest(id: "5", title: "목표달성형퀘스트", content: "", category: .routine, state: .doing),
+            
+            Quest(id: "6", title: "학습퀘스트", content: "", category: .training, state: .todo),
+            Quest(id: "7", title: "목표달성형퀘스트", content: "", category: .routine, state: .todo),
+            Quest(id: "8", title: "목표달성형퀘스트", content: "", category: .routine, state: .todo),
+            Quest(id: "9", title: "목표달성형퀘스트", content: "", category: .routine, state: .todo),
+            Quest(id: "10", title: "목표달성형퀘스트", content: "", category: .routine, state: .todo),
+            Quest(id: "11", title: "목표달성형퀘스트", content: "", category: .routine, state: .todo),
+            
+            Quest(id: "12", title: "학습퀘스트", content: "", category: .training, state: .done),
+            Quest(id: "13", title: "목표달성형퀘스트", content: "", category: .routine, state: .done),
+            Quest(id: "14", title: "목표달성형퀘스트", content: "", category: .routine, state: .done),
+            Quest(id: "15", title: "목표달성형퀘스트", content: "", category: .routine, state: .done),
+            Quest(id: "16", title: "목표달성형퀘스트", content: "", category: .routine, state: .done),
+            Quest(id: "17", title: "목표달성형퀘스트", content: "", category: .routine, state: .done)
         ]
         
         return data
