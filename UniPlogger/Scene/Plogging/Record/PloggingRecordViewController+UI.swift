89b18bfbf014ac1cc192034324f521144f96ca19
ByeonggeunSon <qudrms007@naver.com>
2020-10-22 01:22:08 +0900

    feat: PloggingRecord Scene 생성 및 NotoSans 폰트 추가

---
+++ UniPlogger/Scene/Plogging/Record/PloggingRecordViewController+UI.swift
@@ -0,0 +1,27 @@
+//
+//  PloggingRecordViewController+UI.swift
+//  UniPlogger
+//
+//  Created by 손병근 on 2020/10/22.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+extension PloggingRecordViewController {
+  func configuration() {
+    // backgroundColor = .white
+  }
+  
+  func setupView() {
+    // addSubview(view)
+  }
+  
+  func setupLayout() {
+    // titleLabel.snp.makeConstraints {}
+  }
+  
+  func updateView() {
+    
+  }
+}
