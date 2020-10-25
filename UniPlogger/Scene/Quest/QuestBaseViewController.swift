711adf39c5a25a2e1d79d7bfadb962961a7f1a49
woongs <ccw1021@naver.com>
2020-10-25 17:52:52 +0900

    Add QuestBaseVC for backgroundView
    backgroundView는 statusBar 밑을 기준으로 잡은 뷰

---
+++ UniPlogger/Scene/Quest/QuestBaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  QuestBaseViewController.swift
+//  UniPlogger
+//
+//  Created by woong on 2020/10/25.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+class QuestBaseViewController: BaseViewController {
+
+    var backgroundView = UIView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = Color.questBackground
+    }
+    
+    func setupViews() {
+        view.addSubview(backgroundView)
+    }
+    
+    func setupLayouts() {
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalTo(view.snp.edges).inset(UIEdgeInsets.init(top: UIApplication.shared.statusBarFrame.height, left: 0, bottom: 0, right: 0))
+        }
+    }
+}
