70f5f84f410e457dd4d7d7853b8d22265c8bcd77
ByeonggeunSon <qudrms007@naver.com>
2020-10-28 23:46:11 +0900

    feat: 기록 단위 포멧팅 로직 및 경로 페이스에 따라 여러 색으로 표시하도록 로직 추가

--- UniPlogger/Model/Location/FormatDisplay.swift
+++ UniPlogger/Model/Location/FormatDisplay.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct FormatDisplay{
+  static func distance(_ distance: Double) -> String{
+    let distanceMeasurement = Measurement(value: distance, unit: UnitLength.meters)
+    return FormatDisplay.distance(distanceMeasurement)
+  }
+  
+  static func distance(_ distance: Measurement<UnitLength>) -> String{
+    let formatter = MeasurementFormatter()
+    return formatter.string(from: distance)
+  }
+  
+  static func time(_ seconds: Int) -> String{
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.hour, .minute, .second]
+    formatter.unitsStyle = .positional
+    formatter.zeroFormattingBehavior = .pad
+    return formatter.string(from: TimeInterval(seconds))!
+  }
+  
+  static func pace(distance: Measurement<UnitLength>, seconds: Int, outputUnit: UnitSpeed) -> String{
+    let formatter = MeasurementFormatter()
+    formatter.unitOptions = [.providedUnit]
+    let speedMagnitude = seconds != 0 ? distance.value / Double(seconds) : 0
+    let speed = Measurement(value: speedMagnitude, unit: UnitSpeed.metersPerSecond)
+    return formatter.string(from: speed.converted(to: outputUnit))
+  }
+}
