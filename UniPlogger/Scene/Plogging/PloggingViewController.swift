5b7aaa38936a55b3862632b0e9b73e347ca65435
ByeonggeunSon <qudrms007@naver.com>
2020-10-29 10:35:56 +0900

    feat: 경로 속도별 segments 생성 로직 추가

--- UniPlogger/Scene/Plogging/PloggingViewController.swift
+++ UniPlogger/Scene/Plogging/PloggingViewController.swift
@@ -276,7 +276,75 @@ class PloggingViewController: BaseViewController {
             }
         }
     }
-  
+    private func polyLine(run: Route) -> [MultiColorPolyline] {
+        let locations = run.locations
+        var coordinates: [(CLLocation, CLLocation)] = []
+        var speeds: [Double] = []
+        var minSpeed = Double.greatestFiniteMagnitude
+        var maxSpeed = 0.0
+        
+        for (first, second) in zip(locations, locations.dropFirst()){
+            let start = CLLocation(latitude: first.latitude, longitude: first.longitude)
+            let end = CLLocation(latitude: second.latitude, longitude: second.longitude)
+            coordinates.append((start, end))
+            
+            let distance = end.distance(from: start)
+            let time = second.timestamp.timeIntervalSince(first.timestamp)
+            let speed = time > 0 ? distance / time : 0
+            speeds.append(speed)
+            minSpeed = min(minSpeed, speed)
+            maxSpeed = max(maxSpeed, speed)
+        }
+        
+        let midSpeed = speeds.reduce(0, +) / Double(speeds.count)
+        
+        var segments: [MultiColorPolyline] = []
+        for((start, end), speed) in zip(coordinates, speeds) {
+            let coords = [start.coordinate, end.coordinate]
+            let segment = MultiColorPolyline(coordinates: coords, count: 2)
+            segment.color = segmentColor(
+                speed: speed,
+                midSpeed: midSpeed,
+                slowestSpeed: minSpeed,
+                fastestSpeed: maxSpeed
+            )
+            segments.append(segment)
+        }
+        
+        return segments
+    }
+    
+    private func segmentColor(speed: Double, midSpeed: Double, slowestSpeed: Double, fastestSpeed: Double) -> UIColor {
+      enum BaseColors {
+        static let r_red: CGFloat = 1
+        static let r_green: CGFloat = 20 / 255
+        static let r_blue: CGFloat = 44 / 255
+        
+        static let y_red: CGFloat = 1
+        static let y_green: CGFloat = 215 / 255
+        static let y_blue: CGFloat = 0
+        
+        static let g_red: CGFloat = 0
+        static let g_green: CGFloat = 146 / 255
+        static let g_blue: CGFloat = 78 / 255
+      }
+      
+      let red, green, blue: CGFloat
+      
+      if speed < midSpeed {
+        let ratio = CGFloat((speed - slowestSpeed) / (midSpeed - slowestSpeed))
+        red = BaseColors.r_red + ratio * (BaseColors.y_red - BaseColors.r_red)
+        green = BaseColors.r_green + ratio * (BaseColors.y_green - BaseColors.r_green)
+        blue = BaseColors.r_blue + ratio * (BaseColors.y_blue - BaseColors.r_blue)
+      } else {
+        let ratio = CGFloat((speed - midSpeed) / (fastestSpeed - midSpeed))
+        red = BaseColors.y_red + ratio * (BaseColors.g_red - BaseColors.y_red)
+        green = BaseColors.y_green + ratio * (BaseColors.g_green - BaseColors.y_green)
+        blue = BaseColors.y_blue + ratio * (BaseColors.g_blue - BaseColors.y_blue)
+      }
+      
+      return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 }
 
 extension PloggingViewController: PloggingDisplayLogic{
