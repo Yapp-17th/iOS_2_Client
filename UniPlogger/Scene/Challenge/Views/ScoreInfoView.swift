4577e1600c3c4d907d07be46eee2d95cb87a5096
koserim <btobang1@naver.com>
2020-11-03 01:53:09 +0900

    [feat] ScoreInfoView UI 완성

--- UniPlogger/Scene/Challenge/Views/ScoreInfoView.swift
+++ UniPlogger/Scene/Challenge/Views/ScoreInfoView.swift
@@ -33,6 +33,15 @@ class ScoreInfoView: UIView {
         $0.font = .notoSansKR(ofSize: 12, weight: .regular)
         $0.textAlignment = .center
     }
+    lazy var firstCircleView = CircleInfoView().then {
+        $0.infoLabel.text = "1회\n1000점"
+    }
+    lazy var secondCircleView = CircleInfoView().then {
+        $0.infoLabel.text = "0.01km\n1점\n(1km - 100점)"
+    }
+    lazy var thirdCircleView = CircleInfoView().then {
+        $0.infoLabel.text = "1분\n1점"
+    }
     lazy var characterImageView = UIImageView().then {
         $0.image = UIImage(named: "share_character")
     }
@@ -54,10 +63,11 @@ class ScoreInfoView: UIView {
 extension ScoreInfoView {
     func configure() {
         setDescriptionLabel()
+        setSecondCircleView()
     }
 
     func setUpView() {
-        [backgroundView, infoView, titleView, infoImageView, headerLabel, titleLabel, descriptionLabel, characterImageView].forEach {
+        [backgroundView, infoView, titleView, infoImageView, headerLabel, titleLabel, descriptionLabel, firstCircleView, secondCircleView, thirdCircleView, characterImageView].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             self.addSubview($0)
         }
@@ -96,6 +106,20 @@ extension ScoreInfoView {
             $0.centerX.width.equalTo(infoView)
             $0.height.equalTo(34)
         }
+        secondCircleView.snp.makeConstraints {
+//            $0.top.equalTo(descriptionLabel.snp.bottom).offset(10)
+            $0.centerY.equalTo(infoView).offset(10)
+            $0.centerX.equalTo(infoView)
+            $0.width.height.equalTo(80)
+        }
+        firstCircleView.snp.makeConstraints {
+            $0.top.width.height.equalTo(secondCircleView)
+            $0.trailing.equalTo(secondCircleView.snp.leading).offset(-28)
+        }
+        thirdCircleView.snp.makeConstraints {
+            $0.top.width.height.equalTo(secondCircleView)
+            $0.leading.equalTo(secondCircleView.snp.trailing).offset(28)
+        }
         characterImageView.snp.makeConstraints {
             $0.width.equalTo(100)
             $0.height.equalTo(107.95)
@@ -109,5 +133,12 @@ extension ScoreInfoView {
         attributedString.addAttribute(.font, value: UIFont.notoSansKR(ofSize: 12, weight: .regular), range: (string as NSString).range(of: "같은 점수는 가나다 이름순으로 배치됩니다.\n"))
         attributedString.addAttribute(.font, value: UIFont.notoSansKR(ofSize: 12, weight: .bold), range: (string as NSString).range(of: "가나다 이름순"))
         descriptionLabel.attributedText = attributedString
+    }
+    
+    private func setSecondCircleView() {
+        let string = "0.01km\n1점\n(1km - 100점)"
+        let attributedString = NSMutableAttributedString(string: string)
+        attributedString.addAttribute(.font, value: UIFont.notoSansKR(ofSize: 10, weight: .regular), range: (string as NSString).range(of: "(1km - 100점)"))
+        secondCircleView.infoLabel.attributedText = attributedString
     }
 }
