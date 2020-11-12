7879414b31b04a7326cbaa920035a311902182d5
ByeonggeunSon <qudrms007@naver.com>
2020-11-12 21:36:24 +0900

    feat: 네트워크 로깅(디버깅)을 위한 플러그인 추가

---
+++ UniPlogger/Common/Extension/Data+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Data+Extensions.swift
+//  UniPlogger
+//
+//  Created by 손병근 on 2020/11/12.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
