92b105b45a0b7138c1592ce1be875e52e56cbe44
ByeonggeunSon <qudrms007@naver.com>
2020-11-12 21:58:25 +0900

    feat: Plogging api target 추가 및 플러그인 연결, 쓰레기통 추가, 조회 api 추가

---
+++ UniPlogger/Resources/Network/Plogging/PloggingAPI.swift
@@ -0,0 +1,42 @@
+//
+//  PloggingAPI.swift
+//  UniPlogger
+//
+//  Created by 손병근 on 2020/11/12.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Moya
+import RxSwift
+
+final class PloggingAPI{
+    typealias Response<T: Codable> = BaseResponse<T>
+    
+    let disposeBag = DisposeBag()
+    
+    static let shared = PloggingAPI()
+    private let provider = MoyaProvider<PloggingAPITarget>(
+        session: SessionManager.shared,
+        plugins: [VerbosePlugin(verbose: true)]
+    )
+    
+    func createTrashCan(latitude: Double, longitude: Double, completionHandler: @escaping (Result<BaseResponse<Bool>, Error>) -> Void){
+        provider.rx.request(.createTrash(latitude: latitude, longitude: longitude))
+          .filterSuccessfulStatusCodes()
+          .map(BaseResponse<Bool>.self)
+          .subscribe(onSuccess: {
+            completionHandler(.success($0))
+          }, onError: { completionHandler(.failure($0)) })
+          .disposed(by: disposeBag)
+    }
+    
+    func fetchTrashList(completionHandler: @escaping (Result<BaseResponse<[TrashCan]>, Error>)-> Void){
+        provider.rx.request(.fetchTrashList)
+          .filterSuccessfulStatusCodes()
+          .map(BaseResponse<[TrashCan]>.self)
+          .subscribe(onSuccess: {
+            completionHandler(.success($0))
+          }, onError: { completionHandler(.failure($0)) })
+          .disposed(by: disposeBag)
+    }
+}
