08ae5cf75349904a29fb8609a02d733c43b67828
ByeonggeunSon <qudrms007@naver.com>
2020-11-21 15:13:26 +0900

    feat: 기본 로그 템플릿 추가

--- UniPlogger/Scene/Plogging/Record/PloggingRecordInteractor.swift
+++ UniPlogger/Scene/Plogging/Record/PloggingRecordInteractor.swift
@@ -29,7 +29,7 @@ class PloggingRecordInteractor: PloggingRecordBusinessLogic, PloggingRecordDataS
     
     var presenter: PloggingRecordPresentationLogic?
     var worker: PloggingRecordWorker?
-  
+    
     func fetchRecord() {
         guard let distance = self.distance,
               let seconds = self.seconds,
