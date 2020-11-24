1e2b068a83025cb38cd37193244f2072df5a5715
koserim <btobang1@naver.com>
2020-11-24 22:32:05 +0900

    [init] Create MyPage Scene

---
+++ UniPlogger/Scene/MyPage/MyPagePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MyPagePresenter.swift
+//  UniPlogger
+//
+//  Created by 고세림 on 2020/11/24.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
---
+++ UniPlogger/Scene/MyPage/MyPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyPageViewController.swift
+//  UniPlogger
+//
+//  Created by 고세림 on 2020/11/24.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
