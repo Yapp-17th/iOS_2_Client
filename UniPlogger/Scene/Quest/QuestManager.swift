91d13a071405071cc144d9037944ee0a21087122
woongs <ccw1021@naver.com>
2020-11-28 13:06:35 +0900

    feat: QuestManager 추가
    - post 미구현

--- UniPlogger/Scene/Quest/QuestManager.swift
+++ UniPlogger/Scene/Quest/QuestManager.swift
@@ -10,14 +10,58 @@ import Foundation
 
 protocol QuestManageable {
     var currentTrainingStep: Int { get }
+    func nextStage()
+    func start(quest: Quest)
+    func finish(plogging: PloggingData)
+    func postCompleteIfSuccess(completed proceedingQuest: ProceedingQuest) -> Bool
 }
 
-struct QuestManager: QuestManageable {
+class QuestManager: QuestManageable {
+    var proceedingQuests = [ProceedingQuest]()
+    var questChecker: QuestCheckable
+    
+    init(questChecker: QuestCheckable) {
+        self.questChecker = questChecker
+    }
+    
+    func nextStage() {
+        let nextStage = currentTrainingStep + 1
+        guard nextStage <= 10 else { return }
+        UserDefaults.standard.setValue(nextStage, forKey: trainingStepKey)
+    }
+    
+    func start(quest: Quest) {
+        proceedingQuests.append(ProceedingQuest(id: quest.id))
+    }
+    
+    func finish(plogging: PloggingData) {
+        let completed = questChecker.finish(plogging: plogging)
+        completed.forEach {
+            if postCompleteIfSuccess(completed: $0), $0.id <= 10 {
+                nextStage()
+            }
+        }
+    }
+    
+    @discardableResult
+    func postCompleteIfSuccess(completed proceedingQuest: ProceedingQuest) -> Bool {
+        guard proceedingQuest.id != 6,
+              proceedingQuest.id != 8,
+              proceedingQuest.id != 10
+        else {
+            if proceedingQuest.completeCount >= 3 {
+                // TODO: post
+                return true
+            }
+            return false
+        }
+        // TODO: post
+        return true
+    }
+    
     private let trainingStepKey = "trainingStep"
-    private var trainingStepInDB = 0
     
     var currentTrainingStep: Int {
-        // return UserDefaults.standard.integer(forKey: trainingStepKey)
-        return trainingStepInDB
+        return UserDefaults.standard.integer(forKey: trainingStepKey)
     }
 }
