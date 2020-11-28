1a88756b36419f8c53c9f7544bc9173425a19c89
woongs <ccw1021@naver.com>
2020-11-28 13:10:43 +0900

    feat: PloggingItemType 추가
    - 종류 더 추가 필요

---
+++ UniPlogger/Model/PloggingItemType.swift
@@ -0,0 +1,14 @@
+//
+//  PloggingItemType.swift
+//  UniPlogger
+//
+//  Created by woong on 2020/11/21.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
+
+enum PloggingItemType {
+    case can
+    case plastic
+}
