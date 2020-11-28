9dd420539264655d9054d28feece445de0f11482
woongs <ccw1021@naver.com>
2020-11-28 13:42:19 +0900

    fix: PloggingItemType enum으로 통일

--- UniPlogger/Model/PloggingItemType.swift
+++ UniPlogger/Model/PloggingItemType.swift
@@ -8,7 +8,28 @@
 
 import Foundation
 
-enum PloggingItemType {
-    case can
+enum PloggingItemType: CaseIterable {
+    case paper
+    case cartons
     case plastic
+    case pet
+    case vinyl
+    case glass
+    case can
+    case general
+}
+
+extension PloggingItemType: CustomStringConvertible {
+    var description: String {
+        switch self {
+            case .paper: return "종이"
+            case .cartons: return "종이팩"
+            case .plastic: return "플라스틱"
+            case .pet: return "페트"
+            case .vinyl: return "비닐류"
+            case .glass: return "유리"
+            case .can: return "캔류"
+            case .general: return "일반쓰레기"
+        }
+    }
 }
