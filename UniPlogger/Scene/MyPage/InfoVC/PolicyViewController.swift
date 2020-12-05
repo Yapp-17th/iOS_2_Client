c6266a83de3deebe1ea3a73ba34613c7b43f8792
koserim <btobang1@naver.com>
2020-12-05 21:52:50 +0900

    [feat] 개인정보처리방침 추가

--- UniPlogger/Scene/MyPage/InfoVC/PolicyViewController.swift
+++ UniPlogger/Scene/MyPage/InfoVC/PolicyViewController.swift
@@ -7,15 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
-class PolicyViewController: InfoBaseViewController {
+class PolicyViewController: InfoBaseViewController, WKNavigationDelegate, WKUIDelegate {
+  
+    var webView: WKWebView!
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
+    override func loadView() {
+        super.loadView()
+        
+        let contentController = WKUserContentController()
+        let config = WKWebViewConfiguration()
+        
+        let userScript = WKUserScript(source: "redHeader()", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(userScript)
+        config.userContentController = contentController
+        
+        webView = WKWebView(frame: self.view.bounds, configuration: config)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        self.view.addSubview(webView)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let url = Bundle.main.url(forResource: "policy", withExtension: "html")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
     
-
-
+    @available(iOS 8.0, *)
+    public func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Swift.Void){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let otherAction = UIAlertAction(title: "OK", style: .default, handler: {action in completionHandler()})
+        alert.addAction(otherAction)
+            
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
