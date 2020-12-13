06e2d36470ed9c09c54232be16a6c3fa3ea316b6
koserim <btobang1@naver.com>
2020-12-13 18:09:43 +0900

    [feat] custom camera - 뒤로가기 버튼 추가

--- UniPlogger/Scene/Share/Camera/ImagePreviewViewController.swift
+++ UniPlogger/Scene/Share/Camera/ImagePreviewViewController.swift
@@ -20,7 +20,20 @@ class ImagePreviewViewController: UIViewController, CameraDataStore {
         $0.setImage(UIImage(named: "camera_next"), for: .normal)
         $0.addTarget(self, action: #selector(touchUpNextButton), for: .touchUpInside)
     }
-    
+    lazy var dismissButtonView = UIView().then {
+        $0.backgroundColor = .clear
+        $0.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(touchUpDismissView))
+        $0.addGestureRecognizer(gesture)
+    }
+    lazy var dismissImageView = UIImageView().then {
+        $0.image = UIImage(named: "camera_dismiss")
+    }
+    lazy var dismissLabel = UILabel().then {
+        $0.text = "촬영"
+        $0.textColor = .white
+        $0.font = .notoSans(ofSize: 17, weight: .regular)
+    }
     var capturedImage: UIImage?
     var ploggingData: PloggingData?
     
@@ -55,9 +68,13 @@ class ImagePreviewViewController: UIViewController, CameraDataStore {
         router?.routeToShare()
     }
     
+    @objc func touchUpDismissView() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     func setupViews() {
         self.view.backgroundColor = .black
-        [imageView, nextButton].forEach {
+        [imageView, nextButton, dismissButtonView, dismissLabel, dismissImageView].forEach {
             self.view.addSubview($0)
         }
     }
@@ -73,6 +90,21 @@ class ImagePreviewViewController: UIViewController, CameraDataStore {
             make.width.equalTo(136)
             make.height.equalTo(52)
             make.bottom.equalTo(-view.frame.height * 0.106)
+        }
+        dismissButtonView.snp.makeConstraints{ (make) in
+            make.top.equalTo(44)
+            make.leading.equalTo(9)
+            make.width.equalTo(80)
+            make.height.equalTo(44)
+        }
+        dismissImageView.snp.makeConstraints { (make) in
+            make.leading.centerY.equalTo(dismissButtonView)
+            make.width.equalTo(12)
+            make.height.equalTo(21)
+        }
+        dismissLabel.snp.makeConstraints { (make) in
+            make.leading.equalTo(dismissImageView.snp.trailing).offset(5.05)
+            make.centerY.equalTo(dismissButtonView)
         }
     }
 }
